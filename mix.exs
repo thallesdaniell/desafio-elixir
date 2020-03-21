0f5801dc75c57884c68713de85416e2a923588c9
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-21 14:30:27 -0300

    Alias correction

--- mix.exs
+++ mix.exs
@@ -51,6 +51,6 @@ defmodule Desafio.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     test: ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
