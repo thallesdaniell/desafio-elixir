200e3a472130d674d1bea6aff794e5a6b028a255
thallesdaniel <thallesdaniell@hotmail.com>
2020-03-21 14:24:16 -0300

    Add credo

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule Desafio.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
